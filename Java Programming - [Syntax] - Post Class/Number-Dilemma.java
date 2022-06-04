5bc3e1297d68632f47922e355b56d2be04bfd49b
Pawan Saini <sainipawan2929@gmail.com>
2022-06-04 18:26:29 +0530

    BC

---
+++ Java Programming - [Syntax] - Post Class/Number-Dilemma.java
@@ -0,0 +1,15 @@
+/**
+ *    author:  Abhishek Chauhan
+ *    created:  Tue May 24 10:16:10 IST 2022
+**/
+
+
+static int StringToInt(String S){
+//Enter your code here
+return Integer.parseInt(S);
+}
+
+static String IntToString(int N){
+//Enter your code here
+return Integer.toString(N);
+}
